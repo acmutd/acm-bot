fbd5cdd9a04747d91bffbf1ac33432a1289d6bb4
Jafar <orangecup6@gmail.com>
2020-05-15 16:50:21 -0500

    v0.3.3 | Disabled Google Vision, removed userUpdate

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,9 +2,9 @@ const toolkit = require('../toolkit.js');
 
 module.exports.run = async (client, msg, args) => {
     const avatarURL = msg.author.displayAvatarURL({format: 'png'});
-    const results = await toolkit.ai.detectSafeSearch(avatarURL);
-    msg.channel.send(`Explicit Results: ${results ? results.adult : 'No Results'}`);
-    msg.channel.send(`Explicit Confidence: ${results ? results.adultConfidence : 'No Results'}`);
+    // const results = await toolkit.ai.detectSafeSearch(avatarURL);
+    // msg.channel.send(`Explicit Results: ${results ? results.adult : 'No Results'}`);
+    // msg.channel.send(`Explicit Confidence: ${results ? results.adultConfidence : 'No Results'}`);
 }
 
 module.exports.info = {
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,11 +2,11 @@ const toolkit = require('../toolkit.js');
 
 module.exports.run = async (client, member) => {
     // obscenity detection
-    const results = await toolkit.ai.detectSafeSearch(member.user.avatarURL());
-    if(results.adult == 'LIKELY' || results.adult == 'VERY_LIKELY') {
-        member.ban({ days: 7, reason: 'ACM bot detected a \'LIKELY\' or \'VERY_LIKELY\' explicit profile picture.' })
-    }
-    if(member.user.bot) return;
+    // const results = await toolkit.ai.detectSafeSearch(member.user.avatarURL());
+    // if(results.adult == 'LIKELY' || results.adult == 'VERY_LIKELY') {
+    //     member.ban({ days: 7, reason: 'ACM bot detected a \'LIKELY\' or \'VERY_LIKELY\' explicit profile picture.' })
+    // }
+    // if(member.user.bot) return;
 }
 
 module.exports.info = {
