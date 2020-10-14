e4e800c39d8117c4e6fd41e54d76fdd1192d6f5d
Jafar <jafar.adi.ali@gmail.com>
2020-10-14 00:22:13 -0500

    added smthing idek

--- src/structures/services/ActivityService.ts
+++ src/structures/services/ActivityService.ts
@@ -16,25 +16,32 @@ export default class ActivityService {
     }
 
     async handle(msg: Message) {
-        if (msg.author.bot) return;                             // don't process bots
-        if (msg.content.startsWith(settings.prefix)) return;    // don't count commands
-        if (msg.guild?.id != settings.guild) return;            // don't count anything outside of guild, including DMs
-
+        if (msg.author.bot) return; // don't process bots
+        if (msg.content.startsWith(settings.prefix)) return; // don't count commands
+        if (msg.guild?.id != settings.guild) return; // don't count anything outside of guild, including DMs
+        if (this.enabled === false) return;
         // add points if cooldown has expired or if first message
-        if ( 
-            (this.activityLog.has(msg.author.id) && Number(msg.id) >> 22 > this.activityLog.get(msg.author.id)! + 10*1000) ||
-            (!this.activityLog.has(msg.author.id))
+        if (
+            (this.activityLog.has(msg.author.id) &&
+                Number(msg.id) >> 22 > this.activityLog.get(msg.author.id)! + 10 * 1000) ||
+            !this.activityLog.has(msg.author.id)
         ) {
-            let {success, failure} = await this.client.services.hacktoberfest.awardPoints(1, 'DiscordActivity', new Set<string>(msg.author.id));
+            let { success, failure } = await this.client.services.hacktoberfest.awardPoints(
+                1,
+                'DiscordActivity',
+                new Set<string>(msg.author.id)
+            );
             if (success.length == 0) {
-                ; // put something here if we want to handle user not registered
-            }
-            else {
-                console.log(`${new Date().toLocaleTimeString()}: ${msg.author.tag} was awarded 1 pt for activity (${Number(msg.id) >> 22})`);
+                // put something here if we want to handle user not registered
+            } else {
+                console.log(
+                    `${new Date().toLocaleTimeString()}: ${
+                        msg.author.tag
+                    } was awarded 1 pt for activity (${Number(msg.id) >> 22})`
+                );
             }
             this.activityLog.set(msg.author.id, Number(msg.id) >> 22);
         }
-
     }
 
     enable() {
@@ -44,7 +51,4 @@ export default class ActivityService {
     disable() {
         this.enabled = false;
     }
-
 }
-
-
