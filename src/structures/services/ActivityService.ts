cc7bc660e2fdc309b6d68dde5ea0d979636ba061
Eric Zhang <ericz314271@gmail.com>
2020-10-14 00:24:36 -0500

    fix push to firestore

--- src/structures/services/ActivityService.ts
+++ src/structures/services/ActivityService.ts
@@ -19,13 +19,14 @@ export default class ActivityService {
         if (msg.author.bot) return;                             // don't process bots
         if (msg.content.startsWith(settings.prefix)) return;    // don't count commands
         if (msg.guild?.id != settings.guild) return;            // don't count anything outside of guild, including DMs
+        if (!this.enabled) return;
 
         // add points if cooldown has expired or if first message
         if ( 
             (this.activityLog.has(msg.author.id) && Number(msg.id) >> 22 > this.activityLog.get(msg.author.id)! + 10*1000) ||
             (!this.activityLog.has(msg.author.id))
         ) {
-            let {success, failure} = await this.client.services.hacktoberfest.awardPoints(1, 'DiscordActivity', new Set<string>(msg.author.id));
+            let {success, failure} = await this.client.services.hacktoberfest.awardPoints(1, 'DiscordActivity', new Set<string>([msg.author.id]));
             if (success.length == 0) {
                 ; // put something here if we want to handle user not registered
             }
