34d0d79251d5fcf1e1f732f37a5462fd8a3dce7e
Eric Zhang <36653830+ez314@users.noreply.github.com>
2021-01-07 11:30:26 -0600

    can now vcsnapshot ppl without them knowing 👀

--- src/commands/VCSnapshot.ts
+++ src/commands/VCSnapshot.ts
@@ -8,10 +8,13 @@ import { settings } from '../botsettings';
 export default class VCSnapshot extends Command {
     constructor() {
         super({
-            name: 'vcsnapshot',
-            description: 'take a snapshot of all users in your current voice chanel',
+            name: "vcsnapshot",
+            description: "Take a snapshot of all users in your current voice channel",
+            longDescription: "Take a snapshot of all users in your current voice channel.\n" +
+                "You can also pass in a voice channel ID to take a snapshot of it without having to join.",
             usage: [
-                'vcsnapshot',
+                "vcsnapshot",
+                "vcsnapshot [channel-id]"
             ],
             dmWorks: false,
         });
@@ -22,13 +25,26 @@ export default class VCSnapshot extends Command {
         let attendees: Array<string> = [];
 
         // attempt to resolve the vc
-        voiceChannel = msg.member?.voice.channel;
-        if (!voiceChannel) {
-            return client.response.emit(
-                msg.channel,
-                `Please join a voice channel!`,
-                'invalid'
-            );
+        if (args.length > 0) {
+            let chan = /^\d{17,19}$/.test(args[0]) ? await client.channels.fetch(args[0]) : undefined;
+            if (!chan || chan.type != "voice") {
+                return client.response.emit(
+                    msg.channel,
+                    `Could not resolve that ID into a valid voice channel`,
+                    "invalid"
+                );
+            }
+            voiceChannel = chan as VoiceChannel;
+        }
+        else {
+            voiceChannel = msg.member?.voice.channel;
+            if (!voiceChannel) {
+                return client.response.emit(
+                    msg.channel,
+                    `Please join a voice channel!`,
+                    "invalid"
+                );
+            }
         }
 
         for (const [, member] of voiceChannel.members) {
@@ -39,7 +55,7 @@ export default class VCSnapshot extends Command {
         msg.channel.send(
             `**VC Snapshot for \`${voiceChannel.name}\` requested by ${msg.author}**:\n` + 
             `Member list: ${attendees.join(" ")}\n` + 
-            `Copyable list: \`${attendees.join("` `")}\``,
+            `Copyable list: \` ${attendees.join("` `")}\``,
             {allowedMentions: {users: []}}
         );
     }
