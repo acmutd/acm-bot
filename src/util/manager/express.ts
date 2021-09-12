f8c6efc3bfa8c0cd735f9d9c050e1ca6c181f258
Eric Zhang <36653830+ez314@users.noreply.github.com>
2021-09-12 16:13:00 -0500

    Add express manager
    Express manager will dynamically load in endpoints, much like the
    command manager dynamically loads in commands.
    
    Express config options to support HTTPS removed for the time being, as
    there are no plans to support HTTPS right now.

---
+++ src/util/manager/express.ts
@@ -0,0 +1,58 @@
+import fs from "fs";
+import bodyParser from "body-parser";
+import express, { Express, Request, Response } from "express";
+import http from "http";
+import Bot from "../../api/bot";
+import { settings } from "../../settings";
+import Manager from "../../api/manager";
+
+export default class ExpressManager extends Manager {
+  public app: Express;
+  public port: number;
+
+  public endpointPath: string;
+  public server: http.Server | null = null;
+
+  constructor(bot: Bot) {
+    super(bot);
+    this.app = express();
+
+    this.port = settings.express.port;
+    this.endpointPath = __dirname + "/../../endpoint/";
+  }
+
+  /**
+   * Setup express application and dynamically load endpoints
+   */
+  async init() {
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.raw());
+
+    this.setupEndpoints();
+
+    this.app.listen(this.port, () => {
+      this.bot.logger.info(`Express server started on port ${this.port}!`);
+    });
+  }
+
+  setupEndpoints() {
+    if (!fs.existsSync(this.endpointPath)) {
+      this.bot.logger.error(
+        `No express endpoints found at ${this.endpointPath}!`
+      );
+      return;
+    }
+    const endpointFiles: string[] = fs.readdirSync(this.endpointPath);
+    endpointFiles.forEach((file) => {
+      // TODO: import logic from each file
+    });
+
+    // Log endpoint names
+    this.app._router.stack.forEach((r: any) => {
+      if (r.route && r.route.path) {
+        this.bot.logger.info(`Registered the '${r.route.path}' endpoint!`);
+      }
+    });
+  }
+}
