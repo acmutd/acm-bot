69ffd4b990ceaad43c9a26ff0c44549f9677c4e2
Eric Zhang <36653830+ez314@users.noreply.github.com>
2021-09-12 18:21:34 -0500

    Add events
    Add guildMemberAdd, messageReactionAdd, ready, and voiceStateUpdate
    events back to the bot.

---
+++ src/event/ready.ts
@@ -0,0 +1,22 @@
+import Event from "../api/event";
+import Bot from "../api/bot";
+
+export default class ReadyEvent extends Event {
+  constructor(bot: Bot) {
+    super(bot, "ready");
+  }
+
+  public async emit(bot: Bot) {
+    if (bot.user) {
+      // Print login info
+      bot.logger.info(
+        `Logged in as ${bot.user.username}#${bot.user.discriminator}!`
+      );
+
+      // Set status
+      await bot.user.setActivity(bot.settings.activity.description, {
+        type: bot.settings.activity.type,
+      });
+    }
+  }
+}
